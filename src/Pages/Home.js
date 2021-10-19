2ecf2d3c6f7d45356f11e3e986c79e9a06ebc797
Srijan Shovit <srijanshovit@gmail.com>
2021-10-19 15:50:37 +0530

    banner added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header  from '../Components/Navbar'
+import Banner  from '../Components/Banner'
 
 import Footer from '../Components/Footer'
 const Home = () => {
     return (
         <div>
             <Header/>
+            <Banner/>
             <Footer/>
         </div>
     )
