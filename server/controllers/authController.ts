9861962c7d7eb0ee25da8182acbf0a44aedaff49
Shubham Jha <shubhamjha03563@gmail.com>
2021-10-23 22:00:06 +0530

    'login-user'

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -6,8 +6,29 @@ export const signup = (req: Request, res: Response, next: NextFunction) => {
   let query = `INSERT INTO users SET ?`;
   db.query(query, req.body, (err: any, result: any) => {
     if (err) throw err;
+    res.json({
+      status: 'pass',
+      message: 'Registered successfully.',
+    });
   });
-  res.json({ status: 'pass', message: 'Registered successfully.' });
 };
 
-export const login = (req: Request, res: Response, next: NextFunction) => {};
+export const login = (req: Request, res: Response, next: NextFunction) => {
+  let query = `SELECT * FROM users WHERE email=? AND password=?`;
+  db.query(
+    query,
+    [req.body.email, req.body.password],
+    (err: any, result: any) => {
+      if (err) throw err;
+      if (result.length == 0) {
+        return res.json({
+          status: 'fail',
+          message: 'Wrong credentials.',
+          data: null,
+        });
+      }
+      result[0].password = undefined;
+      res.json({ status: 'pass', message: 'Login succesfull.', data: result });
+    }
+  );
+};
