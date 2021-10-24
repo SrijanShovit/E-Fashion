961f215f4473afc3e2c3b425f7cbccf391829cbf
Mayur Rajuji Kukade <49469931+Mayurkukde84@users.noreply.github.com>
2021-10-24 10:27:03 +0530

    add gallery details

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import Header  from '../Components/Navbar'
-import Banner  from '../Components/Banner'
-import Cards  from '../Components/FeatureCards'
-import ProductsScreen from './ProductScreen'
+import React from "react";
+import Header from "../Components/Navbar";
+import Banner from "../Components/Banner";
+import Cards from "../Components/FeatureCards";
+import ProductsScreen from "./ProductScreen";
 
-import Footer from '../Components/Footer'
-import { Container } from 'react-bootstrap'
+import Footer from "../Components/Footer";
+import { Container } from "react-bootstrap";
 const Home = () => {
-    return (
-        <div>
-            <Header/>
-            <Banner/>
-            <Cards/>
-            
-            <ProductsScreen/>
-            
-            
-            <Footer/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Cards />
+      <Container>
+        <ProductsScreen />
+      </Container>
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
